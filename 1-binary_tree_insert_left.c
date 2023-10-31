a13b8b951140e7ebc18bf28aa6516c99fe19da7f
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-31 10:38:27 +0100

    Insert a left node for another node
    I had to create a new node as the left-child if the parent node already
    has a left-child.
    
    Re-assignment of the parent node left-child to the new left-child doesn't work
    with the deallocation of memory in the `print function`.

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ *                           (parent node)
+ * @parent: A pointer to the node to insert the left-child
+ * @value: The value to store in the new node
+ *
+ * Return: A pointer to the created node
+ *         NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+	int old_child_value;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+
+	/* Check if the parent already has a left-child */
+	if (parent->left)
+	{
+		old_child_value = parent->left->n;
+		new->left = binary_tree_node(new, old_child_value);
+	}
+	else
+		new->left = NULL;
+
+	parent->left = new;
+
+	return (new);
+}
