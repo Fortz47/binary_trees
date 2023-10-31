7ae2f3e906c211e609d976f258cd77bd015cdff2
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-10-31 13:17:00 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,13 @@
 #include "binary_trees.h"
+#include "math.h"
 
 
 /**
+ * count_nodes - recursive helper function
+ * @tree: pointer to root node of binary tree
+ * @count: count variable
+ *
+ * Return: number of nodes in the binary tree
  */
 size_t count_nodes(const binary_tree_t *tree, size_t *count)
 {
@@ -13,15 +19,22 @@ size_t count_nodes(const binary_tree_t *tree, size_t *count)
 	}
 	return (*count);
 }
+
 /**
+ * binary_tree_height - calculates the height of a binary tree
+ * @tree: pointer to the root node of the binary tree
+ *
+ * Return: height of the binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t count = 0;
+	double value;
 	
 	if (tree == NULL)
 		return (0);
 
 	count_nodes(tree, &count);
-	return (count);
+	value = log2((double)count) - 1
+	return ((size_t)value);
 }
