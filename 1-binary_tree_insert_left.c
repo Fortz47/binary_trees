0f3b919d4f3ecb31a94e956f7bc7b4181ed3f033
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-01 11:57:05 +0100

    Fix betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,9 +28,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		old_child = parent->left;
-                /* Make the `new` node the parent of the `old_child` */
+		/* Make the `new` node the parent of the `old_child` */
 		old_child->parent = new;
-                /* Assign the expected left-child */
+		/* Assign the expected left-child */
 		new->left = old_child;
 
 	}
