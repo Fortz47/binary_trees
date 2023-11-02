60b967b5c07591874fbd1b23ee1b7bd112aa6491
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-11-02 06:36:02 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,28 +1,63 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Check if a binary tree is perfect
- * @tree: Pointer to the root node of the binary tree
+ * count_nodes - recursive helper function
+ * @tree: pointer to root node of binary tree
+ * @count: count variable
  *
- * Return: 1 if the tree is perfect, 0 otherwise
+ * Return: number of nodes in the binary tree
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+void count_nodes(const binary_tree_t *tree, size_t *count)
 {
-	size_t height = 0, nodes = 0, expected_nodes = 1;
+	if (tree != NULL)
+	{
+		*count += 1;
+		count_nodes(tree->left, count);
+		count_nodes(tree->right, count);
+	}
+}
+
+/**
+ * tree_height - calculates the height of a binary tree
+ * @tree: pointer to the root node of the binary tree
+ *
+ * Return: height of the binary tree
+ */
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t height, left_path, right_path;
+
+	if (tree == NULL)
+                return (0);
+
+	/* Check if both `left-child` and `right-child` are not NULL */
+	left_path = tree->left ? 1 + tree_height(tree->left) : 0;
+	right_path = tree->right ? 1 + tree_height(tree->right) : 0;
+
+	height = (left_path > right_path) ? left_path : right_path;
+
+	return (height);
+}
+
+/**
+ * binary_tree_is_full - a function that checks if a binary tree is full
+ * @tree: pointer to root node of binary tree
+ *
+ * Return: 0 if tree is NUll or not full else 1
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	size_t count = 0, full_tree, i, level;
 
 	if (tree == NULL)
 		return (0);
 
-	/* Calculate height and expected nodes for a perfect tree */
-	while (tree->left)
-	{
-		height++;
-		expected_nodes = (expected_nodes << 1) + 1;
-		tree = tree->left;
-	}
-
-	/* Calculate the actual number of nodes */
-	nodes = (1 << height) - 1;
-
-	return (nodes == expected_nodes);
+	level = tree_height(tree) + 1;
+        /* evaluates 2**level */
+	i = 1 << level;
+	full_tree = i - 1;
+	count_nodes(tree, &count);
+	if (count != full_tree)
+		return (0);
+	return (1);
 }
