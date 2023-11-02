490c456ec8a9bb09e5426dbd83a9ce812620ce58
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-02 23:28:05 +0100

    Refactor code

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,48 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * check_value - checks if a value already present in a binary search tree
- * @tree: A pointer to the tree to check
- * @value: The value to check its presence
- *
- * Return: 1 if present, or 0 otherwise
- */
-int check_value(bst_t *tree, int value)
-{
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->n == value)
-		return (1);
-
-	if (check_value(tree->left, value))
-		return (1);
-
-	if (check_value(tree->right, value))
-		return (1);
-
-	return (0);
-}
-
-/**
- * get_next_parent - gets the parent for the new node from the tree
- * @tree: A pointer to the root node of the tree
- *
- * Return: The expected node to assign a child to
- */
-bst_t *get_next_parent(bst_t *tree, int value)
-{
-	if (tree->left == NULL || tree->left == NULL)
-		return (tree);
-
-	if (value < tree->n)
-		return (get_next_parent(tree->left, value));
-	if (value > tree->n)
-		return (get_next_parent(tree->right, value));
-}
-
-/**
  * bst_insert - inserts a value in a Binary Search Tree
  * @tree: A double pointer to the root node of the BST to insert the value
  * @value: The value to store in the node to be inserted
@@ -51,39 +9,37 @@ bst_t *get_next_parent(bst_t *tree, int value)
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *next_parent, *new;
-	int present;
+	bst_t *current, *new, *parent = NULL;
 
 	if (tree == NULL)
 		return (NULL);
 
-	if (*tree == NULL)
+	current = *tree;
+	while (current)
 	{
-		new = binary_tree_node(*tree, value);
-		if (new == NULL)
-			return (NULL);
-
-		*tree = new;
-		return (new);
+		parent = current;
+		if (value < current->n)
+			current = current->left;
+		else
+			current = current->right;
 	}
 
-	/* Check if the value already exist in the tree */
-	present = check_value(*tree, value);
-	if (present)
-		return (NULL);
-
-	/* Get the parent to attach the new child to */
-	next_parent = get_next_parent(*tree, value);
-
-	new = binary_tree_node(next_parent, value);
+	/* Create the new node */
+	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
 
 	/* Decide where to attach the child to */
-	if (value >= next_parent->n)
-		next_parent->right = new;
-	else if (value <= next_parent->n)
-		next_parent->left = new;
-
+	if (parent == NULL)
+		*tree = new;
+	else if (value < parent->n)
+		parent->left = new;
+	else if (value > parent->n)
+		parent->right = new;
+	else
+	{
+		free(new);
+		return (NULL);
+	}
 	return (new);
 }
