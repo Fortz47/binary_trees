05ef0b425bdc744f426d49c02bda19e823f08a02
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-03 01:15:00 +0100

    Search BST for a value

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -9,11 +9,21 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-	(void)value;
+	const bst_t *current;
 
 	if (tree == NULL)
 		return (NULL);
 
+	current = tree;
+	while (current)
+	{
+		if (current->n == value)
+			return ((bst_t *)current);
+		if (current->n > value)
+			current = current->left;
+		else if (value > current->n)
+			current = current->right;
+	}
 
-	return ((bst_t *)tree);
+	return (NULL);
 }
